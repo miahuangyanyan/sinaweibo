3b7fb0c1678c175f6cb280d4c3618e8041c32410
miahuangyanyan <245053992@qq.com>
2018-01-18 17:11:24 +0800

    update cookie

---
+++ cookies.py
@@ -0,0 +1,220 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import base64
+import os
+import time
+import json
+from selenium import webdriver
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+import logging
+import requests
+import re
+import json
+import base64
+import time
+import math
+import random
+from PIL import Image
+try:
+    from urllib.parse import quote_plus
+except:
+    from urllib import quote_plus
+
+
+"""
+    输入你的微博账号和密码，可去淘宝买，一元5个。
+    建议买几十个，实际生产建议100+，微博反爬得厉害，太频繁了会出现302转移。
+"""
+myWeiBo = [
+    # ('seng53520470@163.com', '4baspu'),
+    # ('dang66908321@163.com', '5e0txcm'),
+    # ('niao46874268@163.com', '1he3nh'),
+    # ('long64522686@163.com', '4slxxk'),
+    # ('sheng6974063@163.com', '6hdv05o0')
+    ('*******','*******')
+    #('2532476191@qq.com', 'xisan+421')
+]
+
+'''
+3.4
+所有的请求都分析的好了
+模拟请求 一直不成功
+在考虑是哪里出了问题
+以后学了新的知识后 再来更新
+'''
+
+# 构造 Request headers
+agent = 'Mozilla/5.0 (Windows NT 6.2; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.110 Safari/537.36'
+global headers
+headers = {
+    "Host": "passport.weibo.cn",
+    "Connection": "keep-alive",
+    "Upgrade-Insecure-Requests": "1",
+    'User-Agent': agent
+}
+
+session = requests.session()
+# 访问登录的初始页面
+index_url = "https://passport.weibo.cn/signin/login"
+session.get(index_url, headers=headers)
+
+
+def get_su(username):
+    """
+    对 email 地址和手机号码 先 javascript 中 encodeURIComponent
+    对应 Python 3 中的是 urllib.parse.quote_plus
+    然后在 base64 加密后decode
+    """
+    username_quote = quote_plus(username)
+    username_base64 = base64.b64encode(username_quote.encode("utf-8"))
+    return username_base64.decode("utf-8")
+
+
+def login_pre(username):
+    # 采用构造参数的方式
+    params = {
+        "checkpin": "1",
+        "entry": "mweibo",
+        "su": get_su(username),
+        "callback": "jsonpcallback" + str(int(time.time() * 1000) + math.floor(random.random() * 100000))
+    }
+    '''真是日了狗，下面的这个写成 session.get(login_pre_url，headers=headers) 404 错误
+        这条 3.4 号的注释信息，一定是忽略了 host 的变化，真是逗比。
+    '''
+    pre_url = "https://login.sina.com.cn/sso/prelogin.php"
+    headers["Host"] = "login.sina.com.cn"
+    headers["Referer"] = index_url
+    pre = session.get(pre_url, params=params, headers=headers)
+    pa = r'\((.*?)\)'
+    res = [pre.text.encode('utf-8')]#re.findall(pa, pre.text.encode('utf-8'))
+    print res
+    if res == []:
+        print("好像哪里不对了哦，请检查下你的网络，或者你的账号输入是否正常")
+    else:
+        js = json.loads(res[0])
+        if js["showpin"] == 1:
+            headers["Host"] = "passport.weibo.cn"
+            capt = session.get("https://passport.weibo.cn/captcha/image", headers=headers)
+            capt_json = capt.json()
+            capt_base64 = capt_json['data']['image'].split("base64,")[1]
+            with open('capt.jpg', 'wb') as f:
+                f.write(base64.b64decode(capt_base64))
+                f.close()
+            im = Image.open("capt.jpg")
+            im.show()
+            im.close()
+            cha_code = input("请输入验证码\n>")
+            return cha_code, capt_json['data']['pcid']
+        else:
+            return ""
+
+
+def login(username, password, pincode):
+    postdata = {
+        "username": username,
+        "password": password,
+        "savestate": "1",
+        "ec": "0",
+        "pagerefer": "",
+        "entry": "mweibo",
+        "wentry": "",
+        "loginfrom": "",
+        "client_id": "",
+        "code": "",
+        "qq": "",
+        "hff": "",
+        "hfp": "",
+    }
+    if pincode == "":
+        pass
+    else:
+        print type(pincode)
+        postdata["pincode"] = pincode[0]
+        postdata["pcid"] = pincode[1]
+    headers["Host"] = "passport.weibo.cn"
+    headers["Reference"] = index_url
+    headers["Origin"] = "https://passport.weibo.cn"
+    headers["Content-Type"] = "application/x-www-form-urlencoded"
+
+    post_url = "https://passport.weibo.cn/sso/login"
+    login = session.post(post_url, data=postdata, headers=headers)
+    print(login.cookies)
+    print(login.status_code)
+    js = login.json()
+    print(js)
+    uid = js["data"]["uid"]
+    crossdomain = js["data"]["crossdomainlist"]
+    cn = crossdomain["sina.com.cn"]
+    # 下面两个对应不同的登录 weibo.com 还是 m.weibo.cn
+    # 一定要注意更改 Host
+    # mcn = crossdomain["weibo.cn"]
+    # com = crossdomain['weibo.com']
+    headers["Host"] = "login.sina.com.cn"
+    session.get(cn, headers=headers)
+    headers["Host"] = "weibo.cn"
+    ht = session.get("http://weibo.cn/%s/info" % uid, headers=headers)
+    # print(ht.url)
+    print(session.cookies)
+    pa = r'<title>(.*?)</title>'
+    res = re.findall(pa, ht.text.encode('utf-8'))
+    print("你好%s，你正在使用 xchaoinfo 写的模拟登录" % res[0])
+    cookie = session.cookies.get_dict()
+    return json.dumps(cookie)
+    # print(cn, com, mcn)
+
+
+
+
+
+logger = logging.getLogger(__name__)
+logging.getLogger("selenium").setLevel(logging.WARNING)  # 将selenium的日志级别设成WARNING，太烦人
+
+
+
+
+def getCookie(account, password):
+    return get_cookie_from_login_sina_com_cn(account, password)
+
+def get_cookie_from_login_sina_com_cn(account, password):
+    """ 获取一个账号的Cookie """
+    pincode = login_pre(account)
+    return login(account, password, pincode)
+
+
+
+def initCookie(rconn, spiderName):
+    """ 获取所有账号的Cookies，存入Redis。如果Redis已有该账号的Cookie，则不再获取。 """
+    for weibo in myWeiBo:
+        if rconn.get("%s:Cookies:%s--%s" % (spiderName, weibo[0], weibo[1])) is None:  # 'SinaSpider:Cookies:账号--密码'，为None即不存在。
+            cookie = getCookie(weibo[0], weibo[1])
+            if len(cookie) > 0:
+                rconn.set("%s:Cookies:%s--%s" % (spiderName, weibo[0], weibo[1]), cookie)
+    cookieNum = "".join(rconn.keys()).count("SinaSpider:Cookies")
+    logger.warning("The num of the cookies is %s" % cookieNum)
+    if cookieNum == 0:
+        logger.warning('Stopping...')
+        os.system("pause")
+
+
+def updateCookie(accountText, rconn, spiderName):
+    """ 更新一个账号的Cookie """
+    account = accountText.split("--")[0]
+    password = accountText.split("--")[1]
+    cookie = getCookie(account, password)
+    if len(cookie) > 0:
+        logger.warning("The cookie of %s has been updated successfully!" % account)
+        rconn.set("%s:Cookies:%s" % (spiderName, accountText), cookie)
+    else:
+        logger.warning("The cookie of %s updated failed! Remove it!" % accountText)
+        removeCookie(accountText, rconn, spiderName)
+
+
+def removeCookie(accountText, rconn, spiderName):
+    """ 删除某个账号的Cookie """
+    rconn.delete("%s:Cookies:%s" % (spiderName, accountText))
+    cookieNum = "".join(rconn.keys()).count("SinaSpider:Cookies")
+    logger.warning("The num of the cookies left is %s" % cookieNum)
+    if cookieNum == 0:
+        logger.warning("Stopping...")
+        os.system("pause")
+
